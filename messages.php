1c651e6e4e4f05a1679775e611680cbc149d2f55
XIV202 <wintube001ofc@gmail.com>
2024-08-06 14:50:04 +0300

    Create messages.php

---
+++ messages.php
@@ -0,0 +1,19 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $messages = file_get_contents('messages.txt');
+    header('Content-Type: application/json');
+    echo $messages;
+} elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $data = file_get_contents('php://input');
+    $newMessage = json_decode($data, true)['message'];
+    
+    $currentMessages = file_get_contents('messages.txt');
+    $messagesArray = json_decode($currentMessages, true);
+    $messagesArray[] = $newMessage;
+    
+    file_put_contents('messages.txt', json_encode($messagesArray));
+    echo "Сообщение сохранено успешно";
+}
+
+?>
